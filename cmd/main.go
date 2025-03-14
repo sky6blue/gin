6b5d55586023e84fc6d27945fa350b0200568dd9
Nikita Satosov <nikitko2567@gmail.com>
2025-03-14 21:27:04 +0300

    just param route

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -12,11 +13,22 @@ func main() {
 func runApp() {
 	r := gin.Default()
 
+	{
+		v1 := r.Group("/v1")
+		v1.GET("/:name", func(c *gin.Context) {
+
+			c.JSON(http.StatusOK, gin.H{
+				"message": fmt.Sprintf("Hello, %s!", c.Param("name")),
+			})
+		})
+	}
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
 	if err := r.Run("0.0.0.0:9009"); err != nil {
 		panic(err)
 	}
